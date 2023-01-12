94b69bdf7549e2e92fa83a9b924d4b4d526eb1eb
RickTimmer <ricktimmer98@gmail.com>
2023-01-12 11:34:03 +0100

    Added some function templating

--- script.py
+++ script.py
@@ -1,4 +1,16 @@
 
+def text_preproccessing():
+    raise NotImplementedError
+
+def feature_generation():
+    raise NotImplementedError
+
+def ml_classifiers():
+    raise NotImplementedError
+
+def plot_results():
+    raise NotImplementedError
+
 def main():
     print("Hello, World!")
 
